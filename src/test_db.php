ec6a528e4eb7676552eb453c7c3dc69b173d2030
Kirjakauppias <128515579+Kirjakauppias@users.noreply.github.com>
2025-02-07 11:09:46 +0200

    Create test_db.php

---
+++ src/test_db.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'kotisivukone_htmx.railway.internal';
+$user = 'root';
+$pass = 'PNfRKwbpEQGdiVyTgALFlUKTbRtsZgsO';
+$dbName = 'railway';
+$port = 3306;
+
+// Luo yhteys
+$conn = new mysqli($host, $user, $pass, $dbName, $port);
+
+if ($conn->connect_error) {
+    die("❌ Yhteys epäonnistui: " . $conn->connect_error);
+} else {
+    echo "✅ Yhteys MySQL:ään onnistui!";
+}
