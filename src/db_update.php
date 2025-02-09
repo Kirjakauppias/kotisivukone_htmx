65b567d30fd134935a7428861e87b713c6b3c4d6
Kirjakauppias <128515579+Kirjakauppias@users.noreply.github.com>
2025-02-09 13:44:40 +0200

    Update db_update.php

--- src/db_update.php
+++ src/db_update.php
@@ -1,8 +1,8 @@
 <?php
 require_once "database/db_connect.php";
 
-// Päivitetään kaikki NULL tai tyhjät arvot nykyiseen aikaleimaan
-$conn->query("UPDATE USER SET created_at = NOW() WHERE created_at IS NULL OR created_at = ''");
+// Päivitetään kaikki NULL-arvot nykyiseen aikaleimaan
+$conn->query("UPDATE USER SET created_at = NOW() WHERE created_at IS NULL");
 
 // Muutetaan sarake niin, että oletusarvona on CURRENT_TIMESTAMP
 $conn->query("ALTER TABLE USER MODIFY COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
