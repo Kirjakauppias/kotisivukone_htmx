c5506f59165fa9a06dd5cee5908f485357ca7157
Kirjakauppias <128515579+Kirjakauppias@users.noreply.github.com>
2025-02-09 13:22:01 +0200

    Update db_update.php

--- src/db_update.php
+++ src/db_update.php
@@ -1,11 +1,13 @@
 <?php
 require_once "database/db_connect.php";
 
+// Päivitetään olemassa olevat arvot
 $conn->query("UPDATE USER SET status = 'ACTIVE' WHERE status = 'active'");
 $conn->query("UPDATE USER SET role = 'CUSTOMER' WHERE role = 'customer'");
 
-$conn->query("ALTER TABLE USER ALTER COLUMN status SET DEFAULT 'ACTIVE'");
-$conn->query("ALTER TABLE USER ALTER COLUMN role SET DEFAULT 'CUSTOMER'");
+// Muutetaan sarakkeiden tietotyyppi VARCHAR(50), jotta voidaan asettaa oletusarvot
+$conn->query("ALTER TABLE USER CHANGE COLUMN status status VARCHAR(50) NOT NULL DEFAULT 'ACTIVE'");
+$conn->query("ALTER TABLE USER CHANGE COLUMN role role VARCHAR(50) NOT NULL DEFAULT 'CUSTOMER'");
 
 echo "✅ Päivitys valmis!";
 ?>
