31ff84ae8a6eeed88186ac266561f34a56a45f59
Kirjakauppias <128515579+Kirjakauppias@users.noreply.github.com>
2025-02-17 11:15:09 +0200

    user interface

--- src/dashboard.php
+++ src/dashboard.php
@@ -10,26 +10,26 @@ if (!isset($_SESSION['user_id'])) {
     <h2>Tämä on henkilökohtainen sivusi.</h2>
     <div class="user-interface">
         <div class="user-interface-icons">
-            <section>
-                <a href="" alt="omat tiedot"
-                    hx-get="user_edit_modal.php" 
-                    hx-target="#modal-container" 
-                    hx-trigger="click"
-                >
+            <a href="" alt="omat tiedot"
+            hx-get="user_edit_modal.php" 
+            hx-target="#modal-container" 
+            hx-trigger="click"
+            >
+                <section>
                     <img src="images/dashboard/user.svg" alt="user-image">
-                </a>
-                <h3>Muokkaa tietoja</h3>
-            </section>
-            <section>
-                <a href="" alt="omat tiedot"
-                    hx-get="modals/password_modal.php" 
-                    hx-target="#modal-container" 
-                    hx-trigger="click"
-                >
+                    <h3>Muokkaa tietoja</h3>
+                </section>
+            </a>
+            <a href="" alt="omat tiedot"
+            hx-get="modals/password_modal.php" 
+            hx-target="#modal-container" 
+            hx-trigger="click"
+            >
+                <section>
                     <img src="images/dashboard/padlock.svg" alt="padlock-image">
-                </a>
-                <h3>Vaihda salasana</h3>
-            </section> 
+                    <h3>Vaihda salasana</h3>
+                </section> 
+            </a>
         </div>
     </div>
 </div>
