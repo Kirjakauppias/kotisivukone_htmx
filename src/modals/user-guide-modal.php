29f1bcc2ab85bc792e19248991a1d0f361dbbad6
Kirjakauppias <128515579+Kirjakauppias@users.noreply.github.com>
2025-03-19 10:02:18 +0200

    Update user-guide-modal.php

--- src/modals/user-guide-modal.php
+++ src/modals/user-guide-modal.php
@@ -22,13 +22,37 @@
     </ul>
     <h2>Henkilökohtainen sivu, jossa voit tehdä seuraavia asioita:</h2>
     <ul>
-        <li><p></p></li>
-        <li><p></p></li>
-        <li><p></p></li>
-        <li><p></p></li>
-        <li><p></p></li>
-        <li><p></p></li>
-        <li><p></p></li>
-    </ul>      
+        <li><p>Voit luoda oman blogin!</p></li>
+        <li><p>Katsoa omaa blogia.</p></li>
+        <li><p>Muokata blogia tai poistaa blogin.</p></li>
+        <li><p>Luoda uuden julkaisun sekä lisätä kuvan julkaisuun.</p></li>
+        <li><p>Muokata julkaisuja sekä kuvia.</p></li>
+        <li><p>Muokata omia tietoja tai poistaa tilin.</p></li>
+        <li><p>Vaihtaa salasanan.</p></li>
+    </ul>
+    <h2>Blogin luonti</h2>
+      <ul>
+        <li><p>Anna blogillesi nimi.</p></li>
+        <li><p>Kirjoita kuvaus blogillesi.</p></li>
+        <li><p>Blogisi löytyy osoitteesta: tarinanpaikka.up.railway.app/blogit/blogin-nimi</p></li>
+        <li><p>Jos palvelussa on jo saman niminen blogi, osoitteesi perään tulee arvottu numerosarja.</p></li>
+      </ul>
+    <h2>Blogin tietojen muokkaus</h2>
+      <ul>
+        <li><p>Voit vaihtaa blogin nimeä.</p></li>
+        <li><p>Blogisi osoite muuttuu vastaamaan blogin nimeä.</p></li>
+        <li><p>Jos palvelussa on jo saman niminen blogi, blogin osoitteen perään tulee arvottu numerosarja.</p></li>
+        <li><p>Voit vaihtaa blogisi kuvausta.</p></li>
+      </ul>
+    <h2>Blogin poistaminen</h2>
+      <ul>
+        <li><p>Valitse "Muokkaa blogia" -valikko.</p></li>
+        <li><p>Paina "Poista blogi" -painiketta.</p></li>
+        <li><p>Palvelin varmistaa että haluatko poistaa blogin. Paina "kyllä".</p></li>
+        <li><p>Palvelin poistaa kaikki blogin julkaisut.</p></li>
+        <li><p>Palvelin poistaa kaikki blogin kuvat Cloudinarysta.</p></li>
+        <li><p>Palvelin poistaa blogin.</p></li>
+        <li><p>Voit luoda uuden blogin tämän jälkeen.</p></li>
+      </ul>      
   </div>        
 </div><!--/container-->
